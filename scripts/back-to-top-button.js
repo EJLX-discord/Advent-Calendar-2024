58cfca3ab775cd7e24e3f41942b83f6c0b9cd4bf
espaspw <superpanamaworld@gmail.com>
2023-12-30 18:00:50 -0800

    Modify back-to-top button

--- scripts/back-to-top-button.js
+++ scripts/back-to-top-button.js
@@ -3,29 +3,43 @@ const button = document.createElement('button')
 const style = document.createElement('style')
 style.appendChild(document.createTextNode(`
 .back-to-top-btn {
-  font-family: 'Roboto', sans-serif;
   position: fixed;
-  right: 8px;
-  bottom: 8px;
+  right: 32px;
+  bottom: 32px;
   z-index: 10;
-  padding: 5px;
+  width: 48px;
+  height: 48px;
   background: white;
-  border: 1px solid #222;
-  border-radius: 3px;
+  border: 1px solid #888;
+  border-radius: 50%;
   cursor: pointer;
   transition: color 0.1s linear, background 0.1s linear;
-  box-shadow: 0px 1px 4px 0px #c0c0c051;
+  box-shadow: 0px 2px 4px 0px #c0c0c051;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #444;
 }
 
 .back-to-top-btn:hover {
-  background: #222;
+  background: #444;
   color: white;
+}
+
+.back-to-top-text {
+  position: relative;
+  bottom: 2px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 2rem;
 }
 `))
 document.head.appendChild(style)
 
 button.classList.add('back-to-top-btn')
-button.textContent = 'Back to top'
+const span = document.createElement('span')
+span.classList.add('back-to-top-text')
+span.textContent = '∧'
+button.appendChild(span)
 button.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 })
